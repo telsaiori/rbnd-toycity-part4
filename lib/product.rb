f3a3cc7b443d3b2e9129b125330c8c55e2a5fb92
telsaiori <telsaiori@gmail.com>
2016-05-09 13:27:52 +0000

    finished create method

--- lib/product.rb
+++ lib/product.rb
@@ -31,5 +31,5 @@ class Product < Udacidata
     def auto_increment
       @@count_class_instances += 1
     end
-
+    
 end
--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -3,5 +3,15 @@ require_relative 'errors'
 require 'csv'
 
 class Udacidata
-  # Your code goes here!
+    def self.create(attributes = {})
+        #Product.create(brand: "WalterToys", name: "Sticky Notes", price: 34.00)
+        data_path = File.dirname(__FILE__) + "/../data/data.csv"
+        new_product = Product.new(attributes)
+        CSV.open(data_path, "a+") do |csv|
+            csv << [new_product.id, new_product.brand, new_product.name, new_product.price]
+        end
+
+
+    end
+
 end
