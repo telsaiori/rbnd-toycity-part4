10a67384d30577c1328d08cbe1720d9d8a0ea01f
telsaiori <telsaiori@gmail.com>
2016-05-10 08:38:17 +0000

    still got Expected nil to be an instance of Product, not NilClass

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -3,15 +3,26 @@ require_relative 'errors'
 require 'csv'
 
 class Udacidata
+    @@data_path = File.dirname(__FILE__) + "/../data/data.csv"
+    @@products = []
     def self.create(attributes = {})
-        #Product.create(brand: "WalterToys", name: "Sticky Notes", price: 34.00)
-        data_path = File.dirname(__FILE__) + "/../data/data.csv"
         new_product = Product.new(attributes)
-        CSV.open(data_path, "a+") do |csv|
+        CSV.open(@@data_path, "a+") do |csv|
             csv << [new_product.id, new_product.brand, new_product.name, new_product.price]
         end
-
-
+        new_product
+    end
+    
+    def self.all
+        CSV.read(@@data_path, headers: true).each do |product|
+            @@products << self.new(id: product["id"], brand: product["brand"], name: product["product"], price: product["price"])
+        end
+        @@products
+        #CSV.read(@@data_path).drop(1).map{ |product| @@products << self.new(id: product[0], brand: product[1], name: product[2], pricd: product[3])}
+    end
+    
+    def self.first
+        @@products.first
     end
 
 end
