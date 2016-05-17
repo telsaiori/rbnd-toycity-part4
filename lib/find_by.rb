4b0362df18f0f5678ccab9d2bc15a734ceebe2d6
telsaiori <telsaiori@gmail.com>
2016-05-17 19:12:25 +0000

    revise create_finder_methods

--- lib/find_by.rb
+++ lib/find_by.rb
@@ -2,7 +2,7 @@ class Module
   
   def create_finder_methods(*attributes)
     attributes.each do |find|
-      self.send(:define_singleton_method, "find_by_#{find}") do |params = ""|
+      self.send(:define_singleton_method, "find_by_#{find}") do |params|
         all.each do |product|
           #return product if product.find == params
           return product if product.send(find) == params 
